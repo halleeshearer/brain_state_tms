012c1eb97c1a8a2939759e2c89e4b396dbbcc747
Jeff Eilbott <jeilbott@surveybott.com>
2023-04-11 18:05:28 -0400

    icc: n109 and tr data amount option

--- icc.m
+++ icc.m
@@ -1,7 +1,7 @@
 function icc(varargin)
 repoDir = fileparts(mfilename('fullpath'));
 try
-    sub = load(fullfile(repoDir,'data','m2m4_sub_n111.csv'));
+    sub = load(fullfile(repoDir,'data','m2m4_sub_n109.csv'));
 catch
     sub = [];
 end
@@ -9,6 +9,7 @@ p = inputParser;
 p.addParameter('fcDir','/scratch/st-tv01-1/hcp/targets');
 p.addParameter('rois', {'tpj', 'dlpfc', 'pre_sma'});
 p.addParameter('runs', {'REST1', 'REST4', 'MOVIE2','MOVIE4'});
+p.addParameter('tr',[],@(x) isnumeric(x) && numel(x)==1);
 p.addParameter('sub',sub);
 p.addParameter('logfile','');
 p.addParameter('JobStorageLocation','',@ischar);
@@ -28,6 +29,7 @@ if ~isempty(logfile)
 else
     fid = 0;
 end
+tr = inputs.tr;
 % start parpool
 util.start_parpool('numCores',inputs.cores,'JobStorageLocation',inputs.JobStorageLocation);
 % loop over rois
@@ -37,7 +39,7 @@ for i=1:numel(rois)
         % pick runs matching condition
         r = find(startsWith(runs,cond{j}));
         % preallocate arrays
-        tmp = loadData(fcDir, sub(1), runs{end},rois{i});
+        tmp = loadData(fcDir, sub(1), runs{end},rois{i}, tr);
         dim = size(tmp);
         edges = dim(1)*dim(2);
         run1 = zeros(numel(sub),edges,'single');
@@ -46,8 +48,8 @@ for i=1:numel(rois)
         tic
         xprintf(fid,'%s %s\n', rois{i},cond{j});
         parfor s=1:numel(sub)
-            tmp1 = loadData(fcDir, sub(s), runs{r(1)}, rois{i});
-            tmp2 = loadData(fcDir, sub(s), runs{r(2)}, rois{i});
+            tmp1 = loadData(fcDir, sub(s), runs{r(1)}, rois{i}, tr);
+            tmp2 = loadData(fcDir, sub(s), runs{r(2)}, rois{i}, tr);
             run1(s,:) = reshape(tmp1,1,edges);
             run2(s,:) = reshape(tmp2,1,edges);
         end
@@ -63,7 +65,11 @@ for i=1:numel(rois)
         end
         xprintf(fid,'\n\t%d edge iccs calculated in %.1f min',edges,toc/60);
         % save data
-        file = fullfile(fcDir,sprintf('icc_%s_%s_n%d.txt',rois{i},cond{j}, numel(sub)));
+        if isempty(inputs.tr)
+            file = fullfile(fcDir,sprintf('icc_%s_%s_n%d.txt',rois{i},cond{j}, numel(sub)));
+        else
+            file = fullfile(fcDir,sprintf('icc_%s_%s_%dtr_n%d.txt',rois{i},cond{j}, tr, numel(sub)));
+        end
         writematrix(reshape(icc,dim(1:2)),file);
         xprintf(fid,'%s written!',file);
     end
@@ -73,8 +79,12 @@ if fid
 end
 end
 
-function mat = loadData(matDir, sub, run, roi)
+function mat = loadData(matDir, sub, run, roi, tr)
+if isempty(tr)
     mat = load(fullfile(matDir,sprintf('sub%d_%s_%s.csv', sub, run, roi)));
+else
+    mat = load(fullfile(matDir,sprintf('sub%d_%s_%s_%dtr.csv', sub, run, roi, tr)));
+end
 end
 
 function xprintf(fid, varargin)
